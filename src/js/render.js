edb57ed8b06bd0aa46f346176d27d62f4d9180a0
Martin Cervantes <cervantes.martine@gmail.com>
2020-08-17 16:47:01 -0600

    Add information to create an object

---
+++ src/js/render.js
@@ -0,0 +1,23 @@
+import display from './content';
+
+const render = async (data, units = 'imperial') => {
+  const myForeCast = await data;
+
+  const info = {
+    city: myForeCast.name,
+    country: myForeCast.sys.country,
+    temp: Math.round(myForeCast.main.temp),
+    feelsLike: Math.round(myForeCast.main.feels_like),
+    tempMax: Math.round(myForeCast.main.temp_max),
+    tempMin: Math.round(myForeCast.main.temp_min),
+    weather: myForeCast.weather[0].main,
+    icon: myForeCast.weather[0].icon,
+    humidity: myForeCast.main.humidity,
+    wind: myForeCast.wind.speed,
+    units,
+  };
+
+  display(info);
+};
+
+export default render;
